a74960d3566e291e050c062ef32f35b7f56857d0
Klucly <klucly05@gmail.com>
2024-09-12 23:03:22 +0300

    Lab1-2

--- lab1/src/main/java/lab1/NumberOperations.java
+++ lab1/src/main/java/lab1/NumberOperations.java
@@ -1,12 +1,12 @@
 package lab1;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Locale;
-import java.util.function.Function;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+import java.util.function.Function;
 
 
 public class NumberOperations {
