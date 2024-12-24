9a9875fed323bd484f407f3a8857447342e2e5f8
klucly <klucly05@gmail.com>
2024-12-24 15:06:44 +0500

    Added incremental functions for item properties

--- lab3/src/main/java/com/gildedrose/GildedRose.java
+++ lab3/src/main/java/com/gildedrose/GildedRose.java
@@ -11,21 +11,21 @@ class GildedRose {
         for (Item item : items) {
             if (item.name.equals("Aged Brie") || item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (item.quality < 50) {
-                    item.quality = item.quality + 1;
+                    item.incrementQualityUpToThreshold();
                     if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.sellIn < 11 && item.quality < 50) {
-                            item.quality = item.quality + 1;
+                        if (item.sellIn < 11) {
+                            item.incrementQualityUpToThreshold();
                         }
                         
-                        if (item.sellIn < 6 && item.quality < 50) {
-                            item.quality = item.quality + 1;
+                        if (item.sellIn < 6) {
+                            item.incrementQualityUpToThreshold();
                         }
                     }
                 }
             } else {
-                if (item.quality > 0 && !item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.quality = item.quality - 1;
-                    }
+                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    item.decrementQualityDownToThreshold();
+                }
                 
             }
             if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
@@ -33,15 +33,13 @@ class GildedRose {
             }
             if (item.sellIn < 0) {
                 if (item.name.equals("Aged Brie")) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
+                    item.incrementQualityUpToThreshold();
                 } else {
                     if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         item.quality = 0;
                     } else {
-                        if (item.quality > 0 && !item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            item.quality = item.quality - 1;
+                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                            item.decrementQualityDownToThreshold();
                         }
                     }
                 }
